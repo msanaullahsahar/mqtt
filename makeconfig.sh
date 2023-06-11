0816625b89e68541d5d96c03c6d74c4bf45ef10e
Sahar <hello@sahar.pk>
2023-06-11 23:53:14 +0500

    Update makeconfig.sh

--- makeconfig.sh
+++ makeconfig.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Download mosquitto standard conf file.
+echo "Downloading mosquitto.conf file"
 wget https://raw.githubusercontent.com/msanaullahsahar/mqtt/main/mosquitto.conf
 
 # Get the hostname
+echo "Getting hostname"
 hostname=$(hostname)
 
 # Replace 'domainName' with the hostname in the mosquitto.conf file
